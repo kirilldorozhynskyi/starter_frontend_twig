fe13bd5edcda8351d9d18b7b4edf77428407631a
Kyrylo Dorozhynskyi <kyrylo.dorozhynskyi@justdev.org>
2023-06-12 11:51:22 +0200

    [DEV] Add mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,11 @@
 /*
  * File: /src/js/main.js
  * Project: starter_frontend_twig
- * Version: 1.1.6-3
+ * Version: 1.1.7
  * Created Date: Sunday, June 6th 2021, 17:46:00
  * Author: Kirill Dorozhynskyi - kyrylo.dorozhynskyi@justdev.org
  * -----
- * Last Modified: Friday, March 3rd 2023 16:46:25
+ * Last Modified: Monday, June 12th 2023 11:45:40
  * Modified By: Kirill Dorozhynskyi
  * -----
  * Copyright (c) 2023 justDev
@@ -19,6 +19,7 @@
 // Import Packages
 import LazyLoad from 'vanilla-lazyload'
 import AOS from 'aos'
+import './scripts/header.js'
 
 var lazyLoadInstance = new LazyLoad({
 	// Your custom settings go here
@@ -29,3 +30,5 @@ AOS.init({
 	disable: 'phone',
 	once: true
 })
+
+//
