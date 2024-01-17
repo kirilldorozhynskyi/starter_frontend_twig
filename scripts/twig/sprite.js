8a0428f160bf63f01ae47dec7b35b680470a5040
Kyrylo Dorozhynskyi <kyrylo.dorozhynskyi@justdev.org>
2024-01-17 20:35:06 +0100

    [DEV] Add second parameter for sprite

--- scripts/twig/sprite.js
+++ scripts/twig/sprite.js
@@ -1,20 +1,19 @@
 /*
  * File: /scripts/twig/sprite.js
  * Project: starter_frontend_twig
- * Version: 2.0.0
+ * Version: 2.1.1
  * Created Date: Thursday, September 28th 2023, 16:54:21
  * Author: Kirill Dorozhynskyi - kyrylo.dorozhynskyi@justdev.org
  * -----
- * Last Modified: Thursday, September 28th 2023 17:18:03
+ * Last Modified: Wednesday, January 17th 2024 20:34:09
  * Modified By: Kirill Dorozhynskyi
  * -----
- * Copyright (c) 2023 justDev
+ * Copyright (c) 2024 justDev
  */
 
-const twigFunctionsSprite = (id) => {
-	return `<svg class="sprite-icon icon-${id}" aria-hidden="true" focusable="false">
-		<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-${id}"></use>
-		</svg>`
+const twigFunctionsSprite = (id, secondArgument) => {
+	return `<svg class="sprite-icon icon-${id}${secondArgument ? ' ' + secondArgument : ''}" aria-hidden="true" focusable="false">
+	  <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-${id}"></use>
+	</svg>`
 }
-
 export default twigFunctionsSprite
